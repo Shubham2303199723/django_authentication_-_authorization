0c53b1b4fe5b981e373839e833ece0581ca1bfa7
shubham gurjar <shubhamgurjar222@gmail.com>
2024-04-17 08:11:36 +0530

    Register Function Complete

--- django_auth/views.py
+++ django_auth/views.py
@@ -1,13 +1,22 @@
-from django.http import request
+from django.http import request, HttpResponseRedirect
 from django.shortcuts import render
+from auth_app.models import Userform
 
 
 def Home(request):
     return render(request, 'Home.html')
 
 def signup(request):
-    if request.methpd == 'POST':
-        Name = None
+    if request.method == "POST":
+        name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        city = request.POST.get('city')
+        phone_no = request.POST.get('phone_no')
+        dob = request.POST.get('dob')
+        form = Userform(name=name, email=email, password=password, city=city, phone_no=phone_no, dob=dob)
+        form.save()
+        return HttpResponseRedirect('/signin')
     return render(request, 'Sign_up.html')
 
 def signin(request):
