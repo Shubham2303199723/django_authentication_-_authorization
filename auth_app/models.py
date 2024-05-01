42a113cc60779b37638be82f3c36105f70236399
shubham gurjar <shubhamgurjar222@gmail.com>
2024-05-01 08:35:57 +0530

    Username Field Added

--- auth_app/models.py
+++ auth_app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Userform(models.Model):
-    name = models.CharField(max_length=50, default=None)
-    email = models.EmailField(default=None)
-    password = models.CharField(max_length=50, default=None)
-    city = models.CharField(max_length=50, default=None)
-    phone_no = models.CharField(max_length=10, default=None)
-    dob = models.DateField(default=None)
+    username = models.CharField(max_length=50, default=" ")
+    email = models.EmailField(default=" ")
+    password = models.CharField(max_length=50, default=" ")
+    city = models.CharField(max_length=50, default=" ")
+    phone_no = models.CharField(max_length=10, default=" ")
+    dob = models.DateField(null=True, blank=True)
--- django_auth/views.py
+++ django_auth/views.py
@@ -4,34 +4,24 @@ from auth_app.models import Userform
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
-
 def Home(request):
     return render(request, 'Home.html')
 
 def signup(request):
     if request.method == "POST":
-        name = request.POST.get('full_name')
-        username = request.POST.get('email')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
         city = request.POST.get('city')
         phone_no = request.POST.get('phone_no')
         dob = request.POST.get('dob')
-        form1 = Userform(name=name, email=username, password=password, city=city, phone_no=phone_no, dob=dob)
+        form1 = Userform(username=username, email=email, password=password, city=city, phone_no=phone_no, dob=dob)
         form1.save()
         return HttpResponseRedirect('/signup/')
     return render(request, 'Sign_up.html')
 
 def signin(request):
-    if request.method == 'POST':
-        username = request.POST.get('email')
-        password1 = request.POST.get('password')
-        user1 = authenticate(request, email=username, password=password1)
-        if user1 is not None:
-            login(request, user1)
-            return HttpResponse('You Logged In')
-        else:
-            return HttpResponse('User is invalid')
-    return render(request, 'Sign_in.html')
+    pass
 
 def dashboard(request):
     pass
